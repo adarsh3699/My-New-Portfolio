73e51e7c7facb39c654555f530c4b6f8300039ac
adarsh3699 <adarsh3699@gmail.com>
2025-07-18 01:24:58 +0530

    refactor: Adjust styling and layout in Timeline component for improved responsiveness and visual hierarchy
    - Modified height transform range for better scroll effect.
    - Updated padding and margin for timeline items to enhance spacing.
    - Refined positioning of elements for a more cohesive layout.

--- src/components/ui/timeline.tsx
+++ src/components/ui/timeline.tsx
@@ -24,28 +24,28 @@ export const Timeline = ({ data }: { data: TimelineEntry[] }) => {
 		offset: ["start 10%", "end 50%"],
 	});
 
-	const heightTransform = useTransform(scrollYProgress, [-0.1, 1], [0, height]);
+	const heightTransform = useTransform(scrollYProgress, [-0.07, 1], [0, height]);
 	const opacityTransform = useTransform(scrollYProgress, [0, 0.1], [0, 2]);
 
 	return (
-		<div className="w-full font-sans md:px-4 lg:px-10" ref={containerRef}>
+		<div className="w-full font-sans" ref={containerRef}>
 			<div ref={ref} className="relative max-w-7xl mx-auto pb-20">
 				{data.map((item, index) => (
-					<div key={index} className="flex justify-start pt-10 md:pt-20 md:gap-0">
+					<div key={index} className="flex justify-start pt-10 md:pt-30 md:gap-0">
 						<div className="sticky flex flex-col md:flex-row z-40 items-center top-40 self-start max-w-xs lg:max-w-sm md:w-full">
-							<div className="h-10 absolute left-3 md:left-3 w-10 rounded-full bg-white dark:bg-black flex items-center justify-center">
+							<div className="h-10 absolute left-0 w-10 rounded-full bg-white dark:bg-black flex items-center justify-center">
 								<div className="h-4 w-4 rounded-full bg-neutral-200 dark:bg-neutral-800 border border-neutral-300 dark:border-neutral-700 p-2" />
 							</div>
-							<div className="hidden md:block text-3xl md:pl-20 lg:text-5xl font-bold text-neutral-500 dark:text-neutral-500 ">
+							<div className="hidden md:block text-3xl md:pl-18 lg:text-5xl font-bold text-neutral-500 dark:text-neutral-500 ">
 								{item.title}
 							</div>
 						</div>
 
-						<div className="relative pl-20 pr-0 md:pl-0 w-full">
+						<div className="relative pl-15 pr-0 md:pl-0 w-full">
 							<div className="md:hidden block text-2xl mb-4 text-left font-bold text-neutral-500 dark:text-neutral-500">
 								{item.title}
 							</div>
-							{item.content}{" "}
+							{item.content}
 						</div>
 					</div>
 				))}
@@ -53,7 +53,7 @@ export const Timeline = ({ data }: { data: TimelineEntry[] }) => {
 					style={{
 						height: height + "px",
 					}}
-					className="absolute md:left-8 left-8 top-0 overflow-hidden w-[2px] bg-[linear-gradient(to_bottom,var(--tw-gradient-stops))] from-transparent from-[0%] via-neutral-200 dark:via-neutral-700 to-transparent to-[99%]  [mask-image:linear-gradient(to_bottom,transparent_0%,black_10%,black_90%,transparent_100%)] "
+					className="absolute md:left-5 left-5 top-0 overflow-hidden w-[2px] bg-[linear-gradient(to_bottom,var(--tw-gradient-stops))] from-transparent from-[0%] via-neutral-200 dark:via-neutral-700 to-transparent to-[99%]  [mask-image:linear-gradient(to_bottom,transparent_0%,black_10%,black_90%,transparent_100%)] "
 				>
 					<motion.div
 						style={{
